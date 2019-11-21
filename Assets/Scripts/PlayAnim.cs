cfeeaead7a520a8f3722b20f2c9fdc6dfd342a6b
Rumy <rbourkov@gmail.com>
2019-11-21 14:25:18 +0100

    working on environment design

---
+++ Assets/Scripts/PlayAnim.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PlayAnim : MonoBehaviour
+{
+    private bool animStarted = false;
+    private float duration = 0;
+    
+    void Awake()
+    {
+        GetComponent<PlayableDirector>().Stop();
+        GetComponent<AudioSource>().Stop();
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyUp("g"))
+        {
+            Debug.Log("LANDING");
+            GetComponent<PlayableDirector>().Play();
+            GetComponent<AudioSource>().Play();
+            animStarted = true;
+        }
+        if (animStarted)
+        {
+            
+            if (duration < 25.08F)
+            {
+                Debug.Log(duration);
+                duration += Time.deltaTime;
+            }
+            if (duration >= 25.08F)
+            {
+                Debug.Log(duration);
+                GetComponent<AudioSource>().Stop();
+                animStarted = false;
+
+                //spaceship becomes clickable
+
+            }
+        }
+        
+    }
+}
